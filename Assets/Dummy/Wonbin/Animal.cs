ab56e0029592c081ea299cc822585bb3fd3f185c
ImWonbin <dladnjs333@gmail.com>
2020-04-27 18:06:46 +0900

    0427_1

---
+++ Assets/Dummy/Wonbin/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //반응
+    }
+
+    private void OnMouseUp()
+    {
+        //돌아옴
+    }
+}
---
+++ Assets/Dummy/Wonbin/AnimalManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalManager : MonoBehaviour
+{
+    public List<Animal> animals;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
