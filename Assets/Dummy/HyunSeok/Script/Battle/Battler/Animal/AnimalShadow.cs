c550b5801fa13523965e455e63b8b90d20360986
ManaBingsu <43133819+ManaBingsu@users.noreply.github.com>
2020-07-28 21:12:21 +0900

    fix : Shadow rotation issue

--- Assets/Dummy/HyunSeok/Script/Battle/Battler/Animal/AnimalShadow.cs
+++ Assets/Dummy/HyunSeok/Script/Battle/Battler/Animal/AnimalShadow.cs
@@ -7,13 +7,16 @@ namespace Battle
     public class AnimalShadow : MonoBehaviour
     {
         public float firstY;
+        public float minus;
         private void Start()
         {
             firstY = transform.position.y;
+            minus = transform.position.x - transform.parent.transform.position.x;
         }
         private void LateUpdate()
         {
-            transform.position = new Vector3(transform.position.x, firstY);
+            transform.position = new Vector3(transform.parent.transform.position.x + minus, firstY);
+            transform.rotation = Quaternion.Euler(Vector3.zero);
         }
     }
 }
